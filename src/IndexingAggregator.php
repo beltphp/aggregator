1bd5c027100ee9d5369824c6632d944bab94e6b6
Ramon Kleiss <ramonkleiss@gmail.com>
2015-01-11 23:25:25 +0100

    Create the indexing aggregator

---
+++ src/IndexingAggregator.php
@@ -0,0 +1,81 @@
+<?php
+namespace Belt\Aggregator;
+
+/**
+ * The `IndexingAggregator` is an implementation of the `AggregatorInterface`
+ * that wraps another aggregator and allows users to add indexers that are
+ * called after the data is added to the aggregator.
+ *
+ * @author Ramon Kleiss <ramon@apostle.nl>
+ */
+class IndexingAggregator implements AggregatorInterface
+{
+    /** @var AggregatorInterface */
+    private $aggregator;
+
+    /** @var array */
+    private $indexers = array();
+
+    /**
+     * @param AggregatorInterface $aggregator
+     */
+    public function __construct(AggregatorInterface $aggregator)
+    {
+        $this->aggregator = $aggregator;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function add($source, $type, $unique, array $data, \DateTime $timestamp = null)
+    {
+        $item = [
+            'source'    => $source,
+            'type'      => $type,
+            'unique'    => $unique,
+            'data'      => $data,
+            'timestamp' => $timestamp ?: new \DateTime(),
+        ];
+
+        $identifier = $this->aggregator->add(
+            $source,
+            $type,
+            $unique,
+            $data,
+            $timestamp ?: new \DateTime()
+        );
+
+        if (isset($this->indexers[$type])) {
+            array_walk($this->indexers[$type], function ($indexer) use ($item) {
+                call_user_func_array($indexer, [ $item ]);
+            });
+        }
+
+        return $identifier;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function has($identifier, $type = null, $unique = null)
+    {
+        return $this->aggregator->has($identifier, $type, $unique);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function get($identifier, $type = null, $unique = null)
+    {
+        return $this->aggregator->get($identifier, $type, $unique);
+    }
+
+    /**
+     * @param string   $type
+     * @param Callable $indexer
+     */
+    public function register($type, Callable $indexer)
+    {
+        $this->indexers[$type][] = $indexer;
+    }
+}
